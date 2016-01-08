503cb1a04682f48b999bc0f0c02cb6e9fb95a43b
Asher Webb <ashersweb@gmail.com>
2016-01-08 17:28:02 -0500

    dev on login screen

--- www/scripts/application.js
+++ www/scripts/application.js
@@ -7,7 +7,28 @@ function setModal(){
 
 var App = {
 	init: function(){
-
+	    $( ".container-fluid" ).hide();
+	    var splash = document.getElementById( "splash" );
+	    var show = function() {
+	      splash.style.display = "block";
+	      setTimeout( hide, 2150 ); // 2.5 seconds
+	    }
+	    var hide = function() {
+	      $(splash).fadeOut();
+	      //FIX ME: remove line below 
+	      //localStorage.removeItem( 'initial-info-box-flag' );
+	      // if( localStorage.getItem( 'initial-info-box-flag' ) !== null ){
+	      //   var infoBoxOptions = {
+	      //   message: "With this SwingShift Hotels app you get to pick your check-in time! \nAfter you select an available hotel and room you will be asked to select when you would like to check-in.\nYour check out is automatically set to 24 hours later.\n We call it the flex rate. You may just call it freedom!\n",
+	      //   buttonLabel: "Got it!"
+	      //   };
+	      //   supersonic.ui.dialog.alert( "Flex with Swing Shift!", infoBoxOptions ).then( function() {
+	      //     localStorage.setItem( 'initial-info-box-flag' , 'true' );
+	      //   });
+	      // }
+	      $( ".container-fluid" ).fadeIn();
+	    }
+	    show();
 	},
 	test: function(){
 		var testP = document.getElementById('test');
@@ -38,4 +59,4 @@ var App = {
 	}
 };
 
-document.addEventListener('deviceready', App.attachRegisterLink, false)
+document.addEventListener('deviceready', App.init, false)
